12d46867edcb5c25b8a30df245653bcbd41463e2
codewithsubham <59691474+codewithsubham@users.noreply.github.com>
2021-07-01 01:31:16 +0530

    bare structure for both live and vod is readdy

--- src/js/model/player.js
+++ src/js/model/player.js
@@ -17,12 +17,47 @@ export default class Player{
                      <div class="control_container">
                         <div class="top-container">
                         <div id="seekbar-wrapper">
-                            <input type="range"  max="100" value="50" class="slider" id="myRange">
-                            <progress id="bufferProgress" max="100" value="70" class="bufferProgress"></progress>
-                            <progress id="playbackProgress" max="100" value="50" class="playbackProgress"></progress>
+                            <input type="range"  max="100" value="0" class="slider" id="myRange">
+                            <progress id="bufferProgress" max="100" value="0" class="bufferProgress"></progress>
+                            <progress id="playbackProgress" max="100" value="0" class="playbackProgress"></progress>
                         </div>            
                     </div>
                     <div class="bottom-container">
+                    <div class="first_section">
+                    <button class><svg class="create_icon  bg">
+                        <use xlink:href="img/svg/sprite.svg#icon-volume"></use></svg></button>
+                        <input type="range"  max="100" value="50" class="volSeekRange" >
+                
+                </div>
+                <div class="second_section">
+                    <div class="timer" id="curenttime">
+                        00:00:00
+                    </div>
+                    <div class="center_control-container">
+                        <button id="seekBackward"><svg class="create_icon">
+                            <use xlink:href="img/svg/sprite.svg#icon-backward"></use></svg>
+                        </button>
+                        <button class="playPause" id='playPause'><svg class="create_icon ">
+                                <use xlink:href="img/svg/sprite.svg#icon-play"></use></svg>
+                        </button>
+                        <button id="seekForward"><svg class="create_icon">
+                                <use xlink:href="img/svg/sprite.svg#icon-forward"></use>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="timer" id="totaltime">
+                        00:00:00
+                    </div>
+                </div>
+                <div class="third_section">
+
+                    <button  id='pip'><svg class="create_icon ">
+                        <use xlink:href="img/svg/sprite.svg#icon-pip"></use></svg>
+                </button>
+                <button id="settings"><svg class="create_icon  bg">
+                            <use xlink:href="img/svg/sprite.svg#icon-settings"></use></svg>
+                </button>
+                </div>
                     </div>
                 </div>
             </div>
@@ -37,7 +72,9 @@ export default class Player{
 
 
     changeSeekValue(e){
-            video.muted = false;
+           
             document.getElementById("playbackProgress").value =  parseInt(e.target.value);
     }
+
+
 }
